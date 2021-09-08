3aec73d74d6ea155dc1f8fcd85681ead6254f398
Lara_Anabel_Garcia <glaraanabelperez@gmail.com>
2021-09-08 15:12:16 -0300

    Subiendo proyecto helloWorld Subiendo archivos y solucion.

---
+++ labnet2021.helloWorld/labnet2021.helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace labnet2021.helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

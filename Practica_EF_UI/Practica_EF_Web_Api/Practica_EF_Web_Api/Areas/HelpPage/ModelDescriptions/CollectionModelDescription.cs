148e2b52c7fbef2ee4e263bbd276b37af84ec33b
Lara_Anabel_Garcia <glaraanabelperez@gmail.com>
2021-10-01 16:10:26 -0300

    iniciando tp8

---
+++ Practica_EF_UI/Practica_EF_Web_Api/Practica_EF_Web_Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Practica_EF_Web_Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}

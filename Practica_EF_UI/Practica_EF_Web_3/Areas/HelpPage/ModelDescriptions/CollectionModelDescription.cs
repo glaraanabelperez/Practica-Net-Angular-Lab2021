eccdc6329fa70e46de6900ad32a1e3dedd64fea8
Lara_Anabel_Garcia <glaraanabelperez@gmail.com>
2021-10-26 11:27:27 -0300

    borrando proyecto sin uso

--- Practica_EF_UI/Practica_EF_Web_3/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Practica_EF_Web_3.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Practica_EF_UI/Practica_EF_Web_3/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Practica_EF_Web_3.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
